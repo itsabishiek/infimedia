859a84baac725ec5e46f6e7832f598a9d71b3a30
User <s.abishiek007@gmail.com>
2022-03-01 21:27:35 +0530

    login-bugs

--- src/pages/pins/Pins.jsx
+++ src/pages/pins/Pins.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Pins.css";
 
 const Pins = () => {
-  return <div className="pins">Pins</div>;
+  return <div className="pins">Pins Updated!</div>;
 };
 
 export default Pins;
